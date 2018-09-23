137a4f79d272795c0eb35ede68297724884f8f58
hirodemon <jedfakhfekh@gmail.com>
2018-09-23 23:27:01 +0100

    linking

---
+++ HGameEngine/HGameEngine/lib.h
@@ -0,0 +1,13 @@
+#include <iostream>
+//GLEW
+#include <glew.h>
+//GLFW
+#include <glfw3.h>
+//GLM
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc\type_ptr.hpp>
